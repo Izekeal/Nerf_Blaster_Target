471d18f8957685d21e18dd3e24388839cf329369
Christian Groleau <christiangroleau@users.noreply.github.com>
2023-01-24 11:41:41 -0500

    Added comments

--- Nerf_Blaster_Multiple_Targets/Target/Target.cpp
+++ Nerf_Blaster_Multiple_Targets/Target/Target.cpp
@@ -1,9 +1,14 @@
-#include "Arduino.h"
+#include "Arduino.h" // importing this library here makes more sense as you're making use of it directly in this file
 #include "Target.h"
 Target::Target(int targetNum, int ltPin, int ledPin, bool active, bool ledActive, bool shotDetect){
   pinMode(ltPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  _ltPin = ltPin;
+  
+  // heads up in case you see this elsewhere
+  // it is not required (and often left out) but 
+  // these can also be referenced using the 'this' keyword 
+  // (see: https://stackoverflow.com/questions/6779645/use-of-this-keyword-in-c)
+  _ltPin = ltPin;   
   _ledPin = ledPin;
   _active = active;
   _shotDetect = shotDetect;
@@ -19,11 +24,14 @@ else{
 return _shotDetect;
 }
 bool Target::ready(){
+// what does this method return if this condition is false?
 if (digitalRead(_ltPin) == HIGH){
   _active = true;
   return _active;
   }
 }
+
+// these two methods can be combined into one and accept a boolean parameter
 void Target::ledON(){
 digitalWrite(_ledPin, HIGH);
 }
